f96c62a0c525e91ed554911d53ab4e9f519f3c0a
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-14 14:00:36 +0200

    Update GestorAlarmasTest.java

--- test/pruebas/GestorAlarmasTest.java
+++ test/pruebas/GestorAlarmasTest.java
@@ -9,12 +9,65 @@ import org.junit.jupiter.api.Test;
 class GestorAlarmasTest {
 
 	@Test
-	void testInformarAlarma() {
-		Float[] pos = {12.3f, 12.4f};
+	void testInformarAlarma1() {
+		Float[] posAlert = {1.00f, 1.00f};
 		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, pos, "ETSE", new Date(), alarm);
+		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
 		GestorAlarmas GAS = new GestorAlarmas();
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma.");
 	}
-
+	@Test
+	void testInformarAlarma2() {
+		Float[] posAlert = {1.00f, 1.00f};
+		Float[] posUserF = {100.00f, 100.00f};
+		Alarma alarm = new Alarma(1, "Incendio");
+		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
+		MiembroEquipo me = new MiembroEquipo("Pepe","12345678X","p12","p12","pepe@terra.net",123456789,posUserF,"1","activo");
+		GestorAlarmas GAS = new GestorAlarmas();
+		BBDD.obtenerBBDD().getUsuarios().add(me);
+		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma.");
+	}
+	@Test
+	void testInformarAlarma3() {
+		Float[] posAlert = {1.00f, 1.00f};
+		Float[] posUserV = {1.00f, 1.00f};
+		Alarma alarm = new Alarma(1, "Incendio");
+		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
+		MiembroEquipo me = new MiembroEquipo("Pepe","12345678X","p12","p12","pepe@terra.net",123456789,posUserV,"1","activo");
+		GestorAlarmas GAS = new GestorAlarmas();
+		BBDD.obtenerBBDD().getUsuarios().add(me);
+		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma.");
+	}
+	@Test
+	void testInformarAlarma4() {
+		Float[] posAlert = {1.00f, 1.00f};
+		Alarma alarm = new Alarma(1, "Incendio");
+		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
+		Usuario us = new Usuario("Pepe","12345678X","p12","p12","pepe@terra.net",123456789);
+		GestorAlarmas GAS = new GestorAlarmas();
+		BBDD.obtenerBBDD().getUsuarios().add(us);
+		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma.");
+	}
+	@Test
+	void testInformarAlarma5() {
+		Float[] posAlert = {1.00f, 1.00f};
+		Float[] posUserF = {100.00f, 100.00f};
+		Alarma alarm = new Alarma(1, "Incendio");
+		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
+		MiembroUSC musc = new MiembroUSC("Pepe","12345678X","p12","p12","pepe@terra.net",123456789,posUserF);
+		GestorAlarmas GAS = new GestorAlarmas();
+		BBDD.obtenerBBDD().getUsuarios().add(musc);
+		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma.");
+	}
+	@Test
+	void testInformarAlarma6() {
+		Float[] posAlert = {1.00f, 1.00f};
+		Float[] posUserV = {1.00f, 1.00f};
+		Alarma alarm = new Alarma(1, "Incendio");
+		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
+		MiembroUSC musc = new MiembroUSC("Pepe","12345678X","p12","p12","pepe@terra.net",123456789,posUserV);
+		GestorAlarmas GAS = new GestorAlarmas();
+		BBDD.obtenerBBDD().getUsuarios().add(musc);
+		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma.");
+	}
 }
