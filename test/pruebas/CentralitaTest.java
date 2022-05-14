485fa17716e1f3c2758badced20d03d6fc1058be
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-14 13:59:55 +0200

    Update CentralitaTest.java

--- test/pruebas/CentralitaTest.java
+++ test/pruebas/CentralitaTest.java
@@ -4,11 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class CentralitaTest {
 
@@ -19,13 +15,13 @@ class CentralitaTest {
 		Float[] pos = {12.3f, 12.4f};
 		Alarma alarm = new Alarma(1, "Incendio");
 		Alerta alert=new Alerta("Urgente", 1, pos, "ETSE", new Date(), alarm);
-		assertEquals(true,cen.notificarAlerta(alert),"Error al notificar una alerta.");
+		assertTrue(cen.notificarAlerta(alert),"Error al notificar una alerta.");
 	}
 	@Test
 	void testNotificarAlerta2() {
 		GestorAlarmas GAS= new GestorAlarmas();
 		Centralita cen = new Centralita(GAS);
-		assertEquals(true,cen.notificarAlerta(null),"Error al notificar una alerta nula.");
+		assertTrue(cen.notificarAlerta(null),"Error al notificar una alerta nula.");
 	}
 	
 
